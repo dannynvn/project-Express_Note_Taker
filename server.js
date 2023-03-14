ed5e4a7cb9cccd2add8e76f5e21f2dd8037e6a99
Danny Nguyen <dannynvn@gmail.com>
2023-03-13 18:38:04 -0700

    build: added routes to index html

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
+//import packages and declare port
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3001;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+//GET Route for index.html
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET Route for notes.html
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+//listener for port
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
