b1ca87100c325086af7ae50abe46952785165f43
Danny Nguyen <dannynvn@gmail.com>
2023-03-15 21:06:44 -0700

    added post method to write json into db.json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
-let data = require('./db/db.json');
+let allNotes = require('./db/db.json');
 // const api = require('./assets/js/index.js');
 
 const app = express();
@@ -25,16 +25,49 @@ app.get('/api/notes',(req, res) => {
 
 // post and display notes
 app.post('/api/notes', function (req, res) {
+  
+  console.info(`${req.method} request received to add a note`)
+
   const newNote = {
     uuid: uuid(),
     title: req.body.title,
     text: req.body.text
   };
-  data.push(newNote);
-  res.json(data);
-  console.log(newNote);
-  console.log(data);
-})
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const allNotes = JSON.parse(data);
+
+      // Add a new review
+      allNotes.push(newNote);
+
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(allNotes, null, 3),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated notes!')
+      );
+    }
+  });
+});
+
+
+  //create new note object with unique identifier
+  
+
+  //push new note object into dataß
+  
+
+  //write updated data into db.json
+  // fs.writeFile('./db/db.json', JSON.stringify(allNotes), (err, data));
+
+
+ 
 
 
 
@@ -42,15 +75,18 @@ app.post('/api/notes', function (req, res) {
 
 
 
-//GET Route for index.html
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+
+
 
 //GET Route for notes.html
 app.get('/notes', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
+
+//GET Route for index.html
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 //listener for port
 app.listen(PORT, () =>
